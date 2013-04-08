92fc57cbea88b54f809dbeca370c8784b2920a89
Damien Arondel <damien.arondel@free.fr>
2013-04-08 16:12:09 +0200

    Ajout d'une vérification sur le mot de passe de l'utilisateur

--- interactive-teaching/grails-app/domain/interactive/teaching/User.groovy
+++ interactive-teaching/grails-app/domain/interactive/teaching/User.groovy
@@ -4,6 +4,7 @@ class User {
     
     String login
     String password
+    String password2
     String firstName
     String lastName
     
@@ -12,9 +13,16 @@ class User {
     }
 
     static constraints = {
-        login blank: false, unique: true, size: 5..15
-        password blank: false, size: 5..15
-        firstName blank: false
-        lastName blank: false
+        login blank: false, nullable:false, unique: true, size: 5..15
+        password blank: false, nullable: false, size: 5..15, validator: {password, obj ->
+            def password2 = obj.properties['password2']
+            if (password2 == null) return true // skip matching password validation (only important when setting/resetting pass)
+            password2 = password ? true : ['invalid.matchingpasswords']
+        }
+        firstName blank: false, nullable: false
+        lastName blank: false, nullable: false
     }
+    
+    // will not be included in the database model or object relational mapping of GORM
+    static transients = ['password2']
 }
