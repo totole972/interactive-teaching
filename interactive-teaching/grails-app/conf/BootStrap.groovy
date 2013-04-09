df7a5eda5778c7f26b3f811139a652a1e8822024
Damien Arondel <damien.arondel@free.fr>
2013-04-09 15:07:22 +0200

    Ajout d'une page de login

--- interactive-teaching/grails-app/conf/BootStrap.groovy
+++ interactive-teaching/grails-app/conf/BootStrap.groovy
@@ -1,6 +1,14 @@
+import interactive.teaching.Student
+
 class BootStrap {
 
     def init = { servletContext ->
+        def student1 = new Student(
+            email: "damien.arondel@free.fr",
+            password: "unpassword",
+            firstName: "Damien",
+            lastName: "Arondel")
+        student1.save()
     }
     def destroy = {
     }
