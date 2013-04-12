32d90299d202d288839386550ca11d6e8f5cd7b7
Damien Arondel <damien.arondel@free.fr>
2013-04-12 14:37:31 +0200

    Ajout d'un étudiant

--- interactive-teaching/grails-app/conf/BootStrap.groovy
+++ interactive-teaching/grails-app/conf/BootStrap.groovy
@@ -9,7 +9,15 @@ class BootStrap {
             firstName: "Damien",
             lastName: "Arondel")
         student1.save()
+        
+        def student2 = new Student(
+            email: "kevinanatole@yahoo.fr",
+            password: "passe",
+            firstName: "Kevin",
+            lastName: "Anatole")
+        student2.save()
     }
+    
     def destroy = {
     }
 }
