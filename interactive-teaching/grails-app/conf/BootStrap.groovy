c6b8a8bab04ca1236a39a12e240bfe77137d6b53
Kevin ANATOLE <kevinanatole@yahoo.fr>
2013-04-18 10:30:27 +0200

    Revert "bug bootstrap"
    This reverts commit 9fc2f86e126152a90f66b3ee94aa0ef9a4466ec8.

---
+++ interactive-teaching/grails-app/conf/BootStrap.groovy
@@ -0,0 +1,72 @@
+import interactive.teaching.Course
+import interactive.teaching.Enrollment
+import interactive.teaching.Role
+import interactive.teaching.User
+import interactive.teaching.UserRole
+import grails.util.Environment
+
+class BootStrap {
+    def init = { servletContext ->
+        switch (Environment.current) {
+            case Environment.DEVELOPMENT:
+                createTestingUsers()
+                break;
+            case Environment.PRODUCTION:
+                if (!Role.findByAuthority("ROLE_TEACHER")) {
+                    def teacherRole = new Role(authority: "ROLE_TEACHER").save()
+                }
+                if (!Role.findByAuthority("ROLE_STUDENT")) {
+                    def studentRole = new Role(authority: "ROLE_STUDENT").save()
+                }
+                break;
+        }
+    }
+    
+    def destroy = {
+    }
+    
+    void createTestingUsers() {
+        def studentRole = new Role(authority: "ROLE_STUDENT").save()
+        def teacherRole = new Role(authority: "ROLE_TEACHER").save()
+        
+        def samples = [
+            'damien.arondel@free.fr' : [role: studentRole, firstName: 'Damien', lastName: 'Arondel'],
+            'kevinanatole@yahoo.fr' : [role: studentRole, firstName: 'Kevin', lastName: 'Anatole'],
+            'frederic.migeon@irit.fr' : [role: teacherRole, firstName: 'Frederic', lastName: 'Migeon'],
+            'franck.silvestre@irit.fr' : [role: teacherRole, firstName: 'Franck', lastName: 'Silvestre']
+        ]
+        
+        if (!User.list()) {
+            samples.each { email, userAttrs ->
+                def user = new User(
+                    email: email,
+                    password: 'password',
+                    firstName: userAttrs.firstName,
+                    lastName: userAttrs.lastName)
+
+                if (user.validate()) {
+                    println "Creating user ${email}..."
+                    user.save(flush: true)
+                    UserRole.create(user, userAttrs.role)
+                } else {
+                    println "Error in account bootstrap for ${email}!"
+                    user.errors.each { err ->
+                        println err
+                    }
+                }
+            }
+        }
+    }
+    
+    /*def cours1  = new Course(
+     label: "The Great Migeon Show",
+     admin: prof1,
+  )
+  cours1.save()
+
+
+  def en = new Enrollment(course: cours1, student: student1)
+  en.save()
+  en = new Enrollment(course: cours1, student: student2)
+  en.save()*/
+}
