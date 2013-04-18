a4494f79ec2c19af91cae7937644ec2b5cd46768
Damien Arondel <damien.arondel@free.fr>
2013-04-18 11:11:36 +0200

    Theme

---
+++ interactive-teaching/test/unit/interactive/teaching/ThemeTests.groovy
@@ -0,0 +1,17 @@
+package interactive.teaching
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(Theme)
+class ThemeTests {
+
+    void testSomething() {
+       fail "Implement me"
+    }
+}
