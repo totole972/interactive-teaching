a1303f3edc30821924c77b69d81458ffdb0db741
Damien Arondel <damien.arondel@free.fr>
2013-04-20 15:12:47 +0200

    Génération du contrôleur de cours et de ses messages

--- interactive-teaching/grails-app/controllers/interactive/teaching/CourseController.groovy
+++ interactive-teaching/grails-app/controllers/interactive/teaching/CourseController.groovy
@@ -1,12 +1,9 @@
 package interactive.teaching
 
 import java.security.Principal;
+import org.springframework.dao.DataIntegrityViolationException;
 
 class CourseController {
-    
-    static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
-
-    def scaffold = Course
     
     /**
      * Dependency injection for the springSecurityService.
@@ -41,7 +38,9 @@ class CourseController {
                     courses.put(course, false)
                 }
             }
-            themes.put(theme, courses)
+            if (!courses.isEmpty()) {
+                themes.put(theme, courses)
+            }
         }
         // Render the themes
         [themes: themes]
@@ -66,5 +65,90 @@ class CourseController {
         }
         // Render the themes in list view
         render(view: 'list', model: [themes: themes])
+    }
+    
+    def create() {
+        [courseInstance: new Course(params)]
+    }
+    
+    def save() {
+        def courseInstance = new Course(params)
+        if (!courseInstance.save(flush: true)) {
+            render(view: "create", model: [courseInstance: courseInstance])
+            return
+        }
+
+        flash.message = message(code: 'app.course.created')
+        redirect(action: "show", id: courseInstance.id)
+    }
+    
+    def show(Long id) {
+        def courseInstance = Course.get(id)
+        if (!courseInstance) {
+            flash.message = message(code: 'app.course.unfindable')
+            redirect(action: "list")
+            return
+        }
+
+        [courseInstance: courseInstance]
+    }
+
+    def edit(Long id) {
+        def courseInstance = Course.get(id)
+        if (!courseInstance) {
+            flash.message = message(code: 'app.course.unfindable')
+            redirect(action: "list")
+            return
+        }
+
+        [courseInstance: courseInstance]
+    }
+
+    def update(Long id, Long version) {
+        def courseInstance = Course.get(id)
+        if (!courseInstance) {
+            flash.message = message(code: 'app.course.unfindable')
+            redirect(action: "list")
+            return
+        }
+
+        if (version != null) {
+            if (courseInstance.version > version) {
+                courseInstance.errors.rejectValue("version", "default.optimistic.locking.failure",
+                        [message(code: 'course.label', default: 'Course')] as Object[],
+                        "Another user has updated this course while you were editing")
+                render(view: "edit", model: [courseInstance: courseInstance])
+                return
+            }
+        }
+
+        courseInstance.properties = params
+
+        if (!courseInstance.save(flush: true)) {
+            render(view: "edit", model: [courseInstance: courseInstance])
+            return
+        }
+
+        flash.message = message(code: 'app.course.updated')
+        redirect(action: "show", id: courseInstance.id)
+    }
+    
+    def delete(Long id) {
+        def courseInstance = Course.get(id)
+        if (!courseInstance) {
+            flash.message = message(code: 'app.course.unfindable')
+            redirect(action: "list")
+            return
+        }
+
+        try {
+            courseInstance.delete(flush: true)
+            flash.message = message(code: 'app.course.deleted')
+            redirect(action: "list")
+        }
+        catch (DataIntegrityViolationException e) {
+            flash.message = message(code: 'app.course.not.deleted')
+            redirect(action: "show", id: id)
+        }
     }
 }
