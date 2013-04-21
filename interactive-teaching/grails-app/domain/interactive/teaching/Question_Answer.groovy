e190f40ad80f4b85c1199914f1fc84f0aaecde1e
Kevin ANATOLE <kevinanatole@yahoo.fr>
2013-04-21 11:49:57 +0200

    Création de Question Answer

---
+++ interactive-teaching/grails-app/domain/interactive/teaching/Question_Answer.groovy
@@ -0,0 +1,10 @@
+package interactive.teaching
+
+class Question_Answer {
+
+    Question question
+    Answer answer
+
+    static constraints = {
+    }
+}
--- interactive-teaching/test/unit/interactive/teaching/Question_AnswerTests.groovy
+++ interactive-teaching/test/unit/interactive/teaching/Question_AnswerTests.groovy
@@ -8,8 +8,8 @@ import org.junit.*
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
-@TestFor(Session_Question)
-class Session_QuestionTests {
+@TestFor(Question_Answer)
+class Question_AnswerTests {
 
     void testSomething() {
         fail "Implement me"
